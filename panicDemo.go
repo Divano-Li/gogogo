d0cd574cb0cfeb96c81766f9bf3ea005766a3b3a
xulilin <xulilin@wsdashi.com>
2021-07-06 21:15:44 +0800

    commit

---
+++ panicDemo.go
@@ -0,0 +1,25 @@
+package main
+/*
+go中可以抛出一个panic的异常，然后在defer中通过recover捕获这个异常，然后正常处理
+在一个主进程，多个go程处理逻辑的结构中，这个很重要，如果不用recover捕获panic异常，会导致整个进程出错中断
+*/
+import "fmt"
+func main() {
+	defer func() {     //必须要先声明defer，否则不能捕获到panic异常
+		fmt.Println("c")
+		if err := recover(); err != nil {
+			fmt.Println(err)    //这里的err其实就是panic传入的内容，55
+		}
+		fmt.Println("d")
+	}()
+	f()
+}
+
+
+func f() {
+	fmt.Println("a")
+	panic(55)
+	fmt.Println("b")
+
+	fmt.Println("f")
+}
--- quick_sort.go
+++ quick_sort.go
@@ -30,6 +30,45 @@ func quickSort(arr []int, head int, last int) {
 
 func main() {
 	a := []int{5,4,3,2,1}
-	quickSort(a, 0, len(a)-1)
+	quick_sort2(a, 0, len(a)-1)
 	fmt.Println(a)
+	b := map[string]int{"s":1}
+	fmt.Println(b)
+	fmt.Println(b["s"])
+	v,ok:= b["1"]; if ok {
+		fmt.Println(v)
+	} else {
+		fmt.Println(2)
+	}
+	c := []int{1,2,3,4,5}
+	for k,v:= range c {
+		fmt.Println(k)
+		fmt.Println(v)
+	}
+}
+
+func quick_sort2(arr []int, head int, last int) {
+	if head < last {
+		a := arr[head]
+		l, r:= head, last
+		for l<r {
+			for ;l < r && arr[r] >= a; {
+				r--
+			}
+			if l<r {
+				arr[l] = arr[r]
+				l++
+			}
+			for ; l < r && arr[l] <= a; {
+				l++
+			}
+			if l<r {
+				arr[r] = arr[l]
+				r--
+			}
+		}
+		arr[l] = a
+		quick_sort2(arr, head, l-1)
+		quick_sort2(arr, l+1, last)
+	}
 }
