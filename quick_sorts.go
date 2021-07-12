aebfa08a1761d94ecef8a89f6c837b38b9e34359
xulilin <1224160103@qq.com>
2021-07-13 00:37:08 +0800

    百度面试

---
+++ quick_sorts.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"fmt"
+)
+
+func quick_sorts(arr []int, head int, last int) {
+	if head < last {
+		x := arr[head]
+		i, j := head, last
+		for i < j {
+			for i < j && arr[j] >= x {
+				j--
+			}
+			if i < j {
+				arr[i] = arr[j]
+				i++
+			}
+			for i < j && arr[i] <= x {
+				i++
+			}
+			if i < j {
+				arr[j] = arr[i]
+				j--
+			}
+		}
+		arr[i] = x
+		quick_sorts(arr, head, i-1)
+		quick_sorts(arr, i+1, last)
+	}
+}
+
+func main() {
+	a := []int{5, 2, 3, 1, 4}
+	quick_sorts(a, 0, len(a)-1)
+	fmt.Println(a)
+}
+
+type pool struct {
+	MaxNums int
+	Nums    int
+	signal  chan int
+}
+
+func initPool() *pool {
+	return &pool{100, 0, make(chan int, 100)}
+}
+
+func loop() {
+	p := initPool()
+	for {
+		select {
+		case a := <-p.signal:
+			if a == 1 {
+				p.getGoroutine()
+			} else {
+				p.receiveGoroutine()
+			}
+		default:
+		}
+	}
+}
+
+func (p *pool) getGoroutine() bool {
+
+	if p.Nums > p.MaxNums {
+		return false
+	} else {
+		p.Nums = p.Nums + 1
+		return true
+	}
+}
+
+func (p *pool) receiveGoroutine() {
+	p.Nums = p.Nums - 1
+}
